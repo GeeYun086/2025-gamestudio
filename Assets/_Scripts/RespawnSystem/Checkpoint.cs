9d6b173b8897a1304ebdd1039633a613f9701229
Flixl <Felix.Scheuerpflug@gmail.com>
2025-05-06 11:47:00 +0200

    Initial RespawnSystem

---
+++ Assets/_Scripts/RespawnSystem/Checkpoint.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace GravityGame.RespawnSystem
+{
+    public class Checkpoint : MonoBehaviour
+    {
+        [SerializeField]
+        private Material activatedMaterial;
+        [SerializeField]
+        private ParticleSystem activationParticlesPrefab;
+        [SerializeField]
+        private AudioSource activationSound;
+    }
+}
---
+++ Assets/_Scripts/RespawnSystem/RespawnController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace GravityGame.RespawnSystem
+{
+    public class RespawnController : MonoBehaviour
+    {
+        public static RespawnController Instance { get; private set; }
+
+        [SerializeField] GameObject playerObject;
+
+        Vector3 _currentRespawnPosition;
+        Quaternion _currentRespawnRotation;
+        CharacterController _playerCharacterController;
+        
+        void Awake()
+        {
+            Instance = this;
+            _currentRespawnPosition = playerObject.transform.position;
+            _currentRespawnRotation = playerObject.transform.rotation;
+            _playerCharacterController = playerObject.GetComponent<CharacterController>();
+        }
+        
+        void SetCurrentCheckpoint(Vector3 newPos, Quaternion newRot)
+        {
+            _currentRespawnPosition = newPos;
+            _currentRespawnRotation = newRot;
+        }
+        
+        void RespawnPlayer()
+        {
+            _playerCharacterController.enabled = false;
+            
+            playerObject.transform.position = _currentRespawnPosition;
+            playerObject.transform.rotation = _currentRespawnRotation;
+            
+            _playerCharacterController.enabled = true;
+        }
+    }
+}
