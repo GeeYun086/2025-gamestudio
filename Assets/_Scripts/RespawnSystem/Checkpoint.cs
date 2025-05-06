1e695b25be99e8a939fc38da671519356a7efa16
Flixl <Felix.Scheuerpflug@gmail.com>
2025-05-06 16:18:55 +0200

    RespawnSystem #2

--- Assets/_Scripts/RespawnSystem/Checkpoint.cs
+++ Assets/_Scripts/RespawnSystem/Checkpoint.cs
@@ -1,14 +1,45 @@
-﻿using UnityEngine;
+﻿using GravityGame.Player;
+using UnityEngine;
 
 namespace GravityGame.RespawnSystem
 {
+    [RequireComponent(typeof(Collider))]
     public class Checkpoint : MonoBehaviour
     {
+        const string DefaultCheckpointId = "Unique_Checkpoint_Id";
+
         [SerializeField]
-        private Material activatedMaterial;
-        [SerializeField]
-        private ParticleSystem activationParticlesPrefab;
-        [SerializeField]
-        private AudioSource activationSound;
+        string checkpointID = DefaultCheckpointId;
+
+        public bool HasBeenReached { get; private set; }
+        public string CheckpointID => checkpointID;
+
+
+        void Awake()
+        {
+            var tCollider = GetComponent<Collider>();
+            if (!tCollider.isTrigger)
+                tCollider.isTrigger = true;
+
+            if (string.IsNullOrEmpty(checkpointID) || checkpointID == DefaultCheckpointId) {
+                Debug.LogError(
+                    $"[{nameof(Checkpoint)}] '{gameObject.name}' has the default or an empty CheckpointID",
+                    gameObject);
+            }
+        }
+
+        void Start() => RespawnController.RegisterCheckpoint(this);
+
+        void OnDestroy() => RespawnController.UnregisterCheckpoint(this);
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (!other.GetComponentInParent<FirstPersonCameraController>() &&
+                !other.GetComponent<FirstPersonCameraController>())
+                return;
+
+            HasBeenReached = true;
+            RespawnController.Instance.SetCurrentRespawnCheckpoint(this);
+        }
     }
 }
--- Assets/_Scripts/RespawnSystem/RespawnController.cs
+++ Assets/_Scripts/RespawnSystem/RespawnController.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using GravityGame.Player;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace GravityGame.RespawnSystem
 {
@@ -8,32 +12,91 @@ namespace GravityGame.RespawnSystem
 
         [SerializeField] GameObject playerObject;
 
-        Vector3 _currentRespawnPosition;
-        Quaternion _currentRespawnRotation;
+        [SerializeField] string defaultStartingCheckpointID = "InitialSpawn";
+
         CharacterController _playerCharacterController;
-        
+        Checkpoint _currentRespawnTargetCheckpoint;
+
+        private readonly static List<Checkpoint> s_registeredCheckpoints = new List<Checkpoint>();
+        public Checkpoint CurrentlyActiveRespawnPoint => _currentRespawnTargetCheckpoint;
+
+        public List<CheckpointData> GetAllCheckpointData() => s_registeredCheckpoints
+            .Select(cp => new CheckpointData(cp, cp == _currentRespawnTargetCheckpoint)).ToList();
+
         void Awake()
         {
-            Instance = this;
-            _currentRespawnPosition = playerObject.transform.position;
-            _currentRespawnRotation = playerObject.transform.rotation;
+            if (!Instance) {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+                SceneManager.sceneLoaded += OnSceneLoaded;
+            } else if (Instance != this) {
+                Destroy(gameObject);
+                return;
+            }
+            TryFindPlayer();
+        }
+
+        void OnDestroy()
+        {
+            if (Instance == this) SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            TryFindPlayer();
+
+            if (_currentRespawnTargetCheckpoint)
+                return;
+            var defaultCheckpoint = FindCheckpointByID(defaultStartingCheckpointID);
+            if (defaultCheckpoint != null && defaultCheckpoint.HasBeenReached ||
+                defaultCheckpoint != null && string.IsNullOrEmpty(defaultStartingCheckpointID)) {
+                SetCurrentRespawnCheckpoint(defaultCheckpoint);
+            }
+        }
+
+        void TryFindPlayer()
+        {
+            if (!playerObject) {
+                var cameraController = FindFirstObjectByType<FirstPersonCameraController>();
+                if (cameraController) playerObject = cameraController.gameObject;
+            }
             _playerCharacterController = playerObject.GetComponent<CharacterController>();
         }
-        
-        void SetCurrentCheckpoint(Vector3 newPos, Quaternion newRot)
+
+        public void SetCurrentRespawnCheckpoint(Checkpoint newRespawnTarget)
+            => _currentRespawnTargetCheckpoint = newRespawnTarget;
+
+        public void SetActiveRespawnPointByID(string checkpointID)
         {
-            _currentRespawnPosition = newPos;
-            _currentRespawnRotation = newRot;
+            var targetCheckpoint = FindCheckpointByID(checkpointID);
+            if (targetCheckpoint.HasBeenReached) {
+                SetCurrentRespawnCheckpoint(targetCheckpoint);
+            }
         }
-        
-        void RespawnPlayer()
+
+        public void RespawnPlayer()
         {
+            _playerCharacterController = playerObject.GetComponent<CharacterController>();
+
             _playerCharacterController.enabled = false;
-            
-            playerObject.transform.position = _currentRespawnPosition;
-            playerObject.transform.rotation = _currentRespawnRotation;
-            
+            playerObject.transform.position = _currentRespawnTargetCheckpoint.transform.position;
+            playerObject.transform.rotation = _currentRespawnTargetCheckpoint.transform.rotation;
             _playerCharacterController.enabled = true;
         }
+
+        public static void RegisterCheckpoint(Checkpoint checkpoint)
+        {
+            if (s_registeredCheckpoints.Contains(checkpoint)) return;
+            s_registeredCheckpoints.Add(checkpoint);
+        }
+
+        public static void UnregisterCheckpoint(Checkpoint checkpoint)
+        {
+            if (!s_registeredCheckpoints.Remove(checkpoint)) return;
+        }
+
+        static Checkpoint FindCheckpointByID(string id) => string.IsNullOrEmpty(id)
+            ? null
+            : s_registeredCheckpoints.FirstOrDefault(cp => cp.CheckpointID == id);
     }
 }
