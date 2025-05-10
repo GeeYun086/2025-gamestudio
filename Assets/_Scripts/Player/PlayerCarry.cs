3bd9810b2cbd5b8a1f005bf8a937b972613f97c1
VincentSteppert <vincent.steppert@stud-mail.uni-wuerzburg.de>
2025-05-10 14:32:15 +0200

    pick up and release of carryable objects

---
+++ Assets/_Scripts/Player/PlayerCarry.cs
@@ -0,0 +1,41 @@
+using GravityGame.Puzzle_Elements;
+using UnityEngine;
+
+namespace GravityGame.Player
+{
+    /// <summary>
+    ///     Allows the player to pick up <see cref="Carryable" /> objects by
+    ///     1. looking at them and
+    ///     2. pressing the E key
+    ///     Object is then carried in front of the Camera.
+    ///     It can be released by pressing the E key again.
+    /// </summary>
+    public class PlayerCarry : MonoBehaviour
+    {
+        [SerializeField] private Transform playerCameraTransform;
+        [SerializeField] private Transform carryPointTransform;
+        [SerializeField] private LayerMask pickUpLayerMask;
+
+        private Carryable _carrying;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (!_carrying)
+                {
+                    float maxPickUpDistance = 4f;
+                    if (!Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward,
+                            out RaycastHit hit, maxPickUpDistance, pickUpLayerMask)) return;
+                    if (hit.transform.TryGetComponent(out _carrying))
+                        _carrying.PickUp(carryPointTransform);
+                }
+                else
+                {
+                    _carrying.Release();
+                    _carrying = null;
+                }
+            }
+        }
+    }
+}
