f4bb2937c56980b2c19efa2ef1ce8f9e233d6085
Carl Beker <carl.beker@stud-mail.uni-wuerzburg.de>
2025-05-31 21:10:08 +0200

    changed Lever script to InteractableObject and added functionality to change Doors IsPowered by interacting with Lever GameObject

--- Assets/_Scripts/Puzzle Elements/Lever.cs
+++ Assets/_Scripts/Puzzle Elements/Lever.cs
@@ -1,54 +1,45 @@
+using System;
 using System.Collections.Generic;
+using GravityGame.Puzzle_Elements;
 using UnityEngine;
 
 namespace GravityGame
 {
     /// <summary>
-    /// Add to Lever GameObject
+    /// Add to Lever GameObject and drag Doors into List
     /// </summary>
-    public class Lever : RedstoneComponent
+    public class Lever : InteractableObject
     {
+        [Header("Lever Settings")]
         [SerializeField] bool _isPowered;
-
         /// <summary>
         /// Need to Add Door script to doors and drag those GameObjects into this script of the Lever
         /// </summary>
         [SerializeField] public List<RedstoneComponent> LogicComponents = new List<RedstoneComponent>();
-
-        public override bool IsPowered
+        public bool IsPowered
         {
-            get => _isPowered;
-            set
-            {
+            set {
                 _isPowered = value;
                 UpdateConnectedComponents();
             }
         }
 
+        void Awake()
+        {
+            OnInteract.AddListener(() => IsPowered = !_isPowered);
+        }
 
         /// <summary>
         /// Changes IsPowered of all Doors in List.
         /// Switch between on and off.
         /// Need to set in Door`s scripts which Doors are already powered.
         /// </summary>
-        void UpdateConnectedComponents()
+        public void UpdateConnectedComponents()
         {
-            foreach (var component in LogicComponents)
-            {
+            foreach (var component in LogicComponents) {
                 if (component != null)
                     component.IsPowered = !component.IsPowered;
             }
         }
-
-#if UNITY_EDITOR
-        /// <summary>
-        /// Carl: To debug mechanism without the need of playing the level.
-        /// But also updates IsPowered while in Play mode.
-        /// </summary>
-        void OnValidate()
-        {
-            UpdateConnectedComponents();
-        }
-#endif
     }
 }
--- Assets/_Scripts/Puzzle Elements/RedstoneComponent.cs
+++ Assets/_Scripts/Puzzle Elements/RedstoneComponent.cs
@@ -3,10 +3,9 @@ using UnityEngine;
 namespace GravityGame
 {
     /// <summary>
-    /// Interface used for Doors and Levers. Classes that want to change state of one GameObject through another should
-    /// inherit from this.
+    /// Let Doors inherit from this class.
     /// </summary>
-    public abstract class RedstoneComponent : MonoBehaviour
+    public abstract class RedstoneComponent: MonoBehaviour
     {
         public abstract bool IsPowered { get; set; }
     }
