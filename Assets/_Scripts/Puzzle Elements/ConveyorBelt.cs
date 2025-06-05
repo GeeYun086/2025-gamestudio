b6f382c7254fc1e06ab37fb08fd1b6777cb21aee
Flixl <Felix.Scheuerpflug@gmail.com>
2025-06-05 14:53:01 +0200

    i give up

--- Assets/_Scripts/Puzzle Elements/ConveyorBelt.cs
+++ Assets/_Scripts/Puzzle Elements/ConveyorBelt.cs
@@ -6,7 +6,6 @@ namespace GravityGame.Puzzle_Elements
     public class ConveyorBelt : MonoBehaviour
     {
         [SerializeField] float _speed;
-        [SerializeField] float _textureSpeed;
         [SerializeField] Vector3 _direction;
         [SerializeField] List<Rigidbody> _onBelt;
 
@@ -15,12 +14,13 @@ namespace GravityGame.Puzzle_Elements
         void OnValidate() => _direction = _direction.normalized;
         void Start() => _material = GetComponent<MeshRenderer>().material;
 
-        void Update() => _material.mainTextureOffset += new Vector2(1, 0) * (_textureSpeed * Time.deltaTime);
+        // Note FS: This is tested and looks the best
+        void Update() => _material.mainTextureOffset += new Vector2(1, 0) * (_speed/10 * Time.deltaTime);
 
         void FixedUpdate()
         {
             for (int i = 0; i <= _onBelt.Count - 1; i++) {
-                _onBelt[i].AddForce(_speed * _direction, ForceMode.Acceleration);
+                _onBelt[i].AddForce(_speed * _direction, ForceMode.Impulse);
             }
         }
 
