f94f6993e8dd28adcc1739b97b58a17b9f7d448b
Flixl <Felix.Scheuerpflug@gmail.com>
2025-06-15 13:33:24 +0200

    pullPlane instead of pullPoint, fix forceCurve

--- Assets/_Scripts/Puzzle Elements/SpaceHole.cs
+++ Assets/_Scripts/Puzzle Elements/SpaceHole.cs
@@ -6,42 +6,53 @@ namespace GravityGame.Puzzle_Elements
     /// Applies a pull force to Rigidbodies within a defined hemispherical area.
     /// The force strength is modulated by distance using an AnimationCurve.
     /// </summary>
+    [RequireComponent(typeof(Collider))]
     public class SpaceHole : MonoBehaviour
     {
         [SerializeField] float _pullRadius = 20f;
-        [SerializeField] float _pullForce = 20f;
+        [SerializeField] float _pullForce = 50f;
         [SerializeField] LayerMask _affectedLayers = -1;
-        [SerializeField] AnimationCurve _forceCurve = AnimationCurve.Linear(0f, 1f, 1f, 0f);
+        [SerializeField] AnimationCurve _forceCurve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
+
+        float _calculatedHoleRadius;
+
+        void Awake()
+        {
+            var scale = transform.lossyScale;
+            if (TryGetComponent<SphereCollider>(out var sphere)) {
+                _calculatedHoleRadius = sphere.radius * Mathf.Max(scale.x, scale.y, scale.z);
+            } else if (TryGetComponent<BoxCollider>(out var box)) {
+                _calculatedHoleRadius = Mathf.Max(box.size.x * scale.x, box.size.y * scale.y) / 2f;
+            }
+        }
 
         void FixedUpdate()
         {
+            var pullPlane = new Plane(transform.forward, transform.position);
+
             var hitColliders = new Collider[100];
-            int colliders = Physics.OverlapSphereNonAlloc(
+            int collidersFound = Physics.OverlapSphereNonAlloc(
                 transform.position,
                 _pullRadius,
                 hitColliders,
                 _affectedLayers
             );
 
-            for (int i = 0; i < colliders; i++) {
+            for (int i = 0; i < collidersFound; i++) {
                 var rb = hitColliders[i].GetComponent<Rigidbody>();
-                if (rb && rb.gameObject != gameObject && Vector3.Dot(
-                        (hitColliders[i].transform.position - transform.position).normalized,
-                        transform.TransformDirection(Vector3.forward.normalized)
-                    ) > 0) {
-                    var pullVector = transform.position - rb.position;
-                    pullVector.Normalize();
+                if (!rb || rb.gameObject == gameObject) continue;
+                if (Vector3.Dot(rb.position - transform.position, transform.forward) <= 0) continue;
 
-                    float forceMultiplier = 1.0f;
-                    if (_forceCurve != null && _forceCurve.keys.Length > 0) {
-                        forceMultiplier = _forceCurve.Evaluate(Mathf.Clamp01(pullVector.magnitude / _pullRadius));
-                    }
+                var vectorFromCenter = pullPlane.ClosestPointOnPlane(rb.position) - transform.position;
+                var target = vectorFromCenter.sqrMagnitude > _calculatedHoleRadius * _calculatedHoleRadius
+                    ? transform.position + vectorFromCenter.normalized * _calculatedHoleRadius
+                    : pullPlane.ClosestPointOnPlane(rb.position);
 
-                    rb.AddForce(
-                        pullVector * (_pullForce * forceMultiplier),
-                        ForceMode.Acceleration
-                    );
-                }
+                var pullVector = target - rb.position;
+                rb.AddForce(
+                    pullVector.normalized * (_pullForce * _forceCurve.Evaluate(1f - Mathf.Clamp01(pullVector.magnitude / _pullRadius))),
+                    ForceMode.Acceleration
+                );
             }
         }
 
@@ -49,7 +60,6 @@ namespace GravityGame.Puzzle_Elements
         {
             Gizmos.color = Color.cyan;
             Gizmos.DrawWireSphere(transform.position, _pullRadius);
-            Gizmos.DrawLine(transform.position, transform.TransformDirection(Vector3.forward));
         }
     }
 }
