3ad9bd60cb0bf0650efce08a1b5fe9c42bfe58bd
Sol <warnardo@mfunky.de>
2025-06-18 13:23:10 +0200

    Added a greybox label
    Can be used to describe what a greybox is supposed to be.
    Use for missing features or ambiguos but important parts of a level.
    Also has a script with a TextArea that allows for further explanation in the inspector if required.

---
+++ Assets/_Scripts/Utils/GreyboxExplanation.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace GravityGame.Utils
+{
+    public class GreyboxDescriptionExplanation : MonoBehaviour
+    {
+        [Header("Greybox Explanation")]
+        [TextArea]
+        public string _;
+    }
+}
