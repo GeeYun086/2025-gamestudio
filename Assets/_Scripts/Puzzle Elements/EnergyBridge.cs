cc35100a6ab2fe1d39dcb9d0a077b959d1fd9b45
Sol <warnardo@mfunky.de>
2025-06-18 12:50:24 +0200

    Added RedstoneComponent to EnergyBridge
    Added RedstoneComponent, also fixed bridge not working when rotated

--- Assets/_Scripts/Puzzle Elements/EnergyBridge.cs
+++ Assets/_Scripts/Puzzle Elements/EnergyBridge.cs
@@ -6,28 +6,31 @@ namespace GravityGame.Puzzle_Elements
     /// IsActive controls wether the bridge is extended or not.
     /// Changing that value will cause the bridge to extend or retract smoothly.
     /// </summary>
-    public class EnergyBridge : MonoBehaviour
+    public class EnergyBridge : RedstoneComponent
     {
         float _bridgeLength;
-        float _bridgePosition;
-        public bool IsActive = true;
-        
-        
+        Vector3 _bridgePosition;
+        Vector3 _bridgeDirection;
+        public override bool IsPowered { get; set; }
+
+
         void Awake()
         {
             _bridgeLength = transform.localScale.x;
-            _bridgePosition = transform.localPosition.x;
+            _bridgePosition = transform.localPosition;
+            _bridgeDirection = transform.localRotation * Vector3.right;
         }
 
         void Update()
         {
             float currentLength = transform.localScale.x;
-            float targetLength = IsActive ? _bridgeLength : 0f;
+            float targetLength = IsPowered ? _bridgeLength : 0f;
             if (Mathf.Abs(currentLength - targetLength) > 0.01f)
             {
                 float newLength = Mathf.Lerp(currentLength, targetLength, Time.deltaTime * 5f);
                 transform.localScale = new Vector3(newLength, transform.localScale.y, transform.localScale.z);
-                transform.localPosition = new Vector3(_bridgePosition + (newLength - _bridgeLength) / 2f, transform.localPosition.y, transform.localPosition.z);
+                float offset = (newLength - _bridgeLength) / 2f;
+                transform.localPosition = _bridgePosition + _bridgeDirection * offset;
             }
         }
     }
